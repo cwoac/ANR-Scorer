4dba4f6c3d787a59686f32b8025bebf91555b4a9
Oliver Matthews <ojm@codersoffortune.net>
2016-11-06 16:26:42 +0000

    Skeleton TK entry point

---
+++ ANR-Scorer.py
@@ -0,0 +1,13 @@
+"""Main Entry point for ANR Scorer"""
+
+import tkinter as Tk
+
+
+def main():
+    root = Tk.Tk()
+    root.wm_title("ANR Scorer")
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
